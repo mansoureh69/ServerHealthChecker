fa497f282ad7dee15aa9c260d1dc5e526ddc96d6
mansoureh69 <mansourehesmaeili69@gmail.com>
2019-11-14 05:55:20 +0330

    commitService

---
+++ ServerHealthCheckerDomain/HealthCherckerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerHealthCheckerDomain
+{
+     public class HealthCheckerService
+    {
+        public IHealthChecker HealthChecker { get; set; }
+
+        public void Act(Server server)
+        {
+           var healthCheckResult= HealthChecker.ServerHealthCheck(server);
+
+            if(!healthCheckResult)
+                HealthChecker.NotifyToClient(server);
+
+        }
+    }
+}
